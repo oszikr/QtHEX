d24934a34bdca161a2a9a2dcd0e0fca4136cb27b
ezoszkr <krisztian.oszi@ericsson.com>
2018-08-28 12:14:24 +0200

    More comments.

--- hexstrategycontrol.cpp
+++ hexstrategycontrol.cpp
@@ -4,6 +4,8 @@ HexStrategyControl::HexStrategyControl(const HexStateSpace& hex, const HexStateS
     :hex(hex), A(A), B(B)
 {}
 
+// Start game tree generator for all EMPTY fields. If a winning strategy found, return with the field index.
+// Ff the return value of the recursions equals with the current player, this a winning way
 short int HexStrategyControl::getWinningStep() const
 {
     std::cout << ">>> Searching winning strategy for all empty field: " << std::endl;
@@ -11,9 +13,9 @@ short int HexStrategyControl::getWinningStep() const
     for (unsigned short int i = 0; i < hex.getLength(); i++)
     {
         std::cout << "|";
-        if (hex.get(i) == HexStateSpace::EMPTY) {
-            HexStateSpace nextHex(hex);
-            nextHex.set(i, A);
+        if (hex.get(i) == HexStateSpace::EMPTY) { // Empty Field
+            HexStateSpace nextHex(hex); // Copy the state of the table
+            nextHex.set(i, A); // Mark the field for the current player
             HexStateSpace::color winner = alphaBetaRecursion(nextHex, 1);
             if (winner == A)
             {
@@ -26,13 +28,12 @@ short int HexStrategyControl::getWinningStep() const
     return -1;
 }
 
-
+// DFS in the game tree. Alpha/beta cutting is applyed
 HexStateSpace::color HexStrategyControl::alphaBetaRecursion(HexStateSpace& curHex, const unsigned int &level) const
 {
     // In the winner state
     HexStateSpace::color winner = curHex.isWinner();
-    if (winner == A) return A;
-    if (winner == B) return B;
+    if (winner != HexStateSpace::EMPTY) return winner;
 
     // recursion
     if (level % 2 != 0) { // B player's level
